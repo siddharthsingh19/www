e6530c9f6752dd38f56685956e9b3961f0913044
Siddharth Singh <siddharth.singh19@outlook.com>
2023-11-08 12:44:04 +0530

    added more projects

--- src/components/portfolioComp/header/HeaderSocials.jsx
+++ src/components/portfolioComp/header/HeaderSocials.jsx
@@ -3,7 +3,7 @@ import {BsLinkedin, BsGithub,BsDribbble} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target='_blank'><BsLinkedin /></a>
+        <a href="https://linkedin.com/in/siddharth-singh-60547728b" target='_blank'><BsLinkedin /></a>
         <a href="https://github.com/siddharthsingh19" target='_blank'><BsGithub /></a>
         <a href="https://dribble.com" target='_blank'><BsDribbble/></a>
     </div>
--- src/projects.js
+++ src/projects.js
@@ -3,37 +3,69 @@ import bookkeeper from "./assets/bookkeeper.png";
 import redbox from "./assets/redbox.png";
 import snapkart from "./assets/snapkart.png";
 import supersheet from "./assets/supersheet.png";
+import twinkle from "./assets/twinkle.png";
+import hemang from "./assets/hemang.png";
+import easybreathe from "./assets/easybreathe.png";
+import aelia from "./assets/aelia.png";
 const projects = [
     {
         id: 1,
-        image: snapkart,
-        title: "Snapkart",
-        github: "https://github.com/siddharthsingh19/snapkart.git",
-        demo: "https://snapkart.vercel.app/",
+        image: aelia,
+        title: "Aelia Productions",
+        github: "https://github.com/siddharthsingh19/",
+        demo: "https://www.aeliaproduction.com/",
     },
     {
         id: 2,
-        image: supersheet,
-        title: "SuperSheet",
-        github: "https://github.com/siddharthsingh19/supersheet.git",
-        demo: "https://supersheet.vercel.app/",
-    },
-    {
-        id: 3,
         image: redbox,
         title: "Redbox",
         github: "https://github.com/siddharthsingh19/redbox.git",
         demo: "https://redbox-mu.vercel.app/",
     },
     {
+        id: 3,
+        image: hemang,
+        title: "Hemang Dental Clinic",
+        github: "https://github.com/siddharthsingh19/",
+        demo: "https://hemangdentalclinics.com/",
+    },
+    {
         id: 4,
+        image: easybreathe,
+        title: "Easy Breathe Solutions",
+        github: "https://github.com/siddharthsingh19/",
+        demo: "https://easybreathesolution.com/",
+    },
+    {
+        id: 5,
         image: bookkeeper,
         title: "The Book Keeper",
         github: "https://github.com/siddharthsingh19/TheBookKeeper.git",
         demo: "https://the-book-keeper.vercel.app/",
     },
     {
-        id: 5,
+        id: 6,
+        image: twinkle,
+        title: "Twinkle Dental Care",
+        github: "https://github.com/siddharthsingh19/",
+        demo: "https://twinkledentalcare.com/",
+    },
+    {
+        id: 7,
+        image: snapkart,
+        title: "Snapkart",
+        github: "https://github.com/siddharthsingh19/snapkart.git",
+        demo: "https://snapkart.vercel.app/",
+    },
+    {
+        id: 8,
+        image: supersheet,
+        title: "SuperSheet",
+        github: "https://github.com/siddharthsingh19/supersheet.git",
+        demo: "https://supersheet.vercel.app/",
+    },
+    {
+        id: 9,
         image: checklist,
         title: "Checklist",
         github: "https://github.com/siddharthsingh19/checklist.git",
