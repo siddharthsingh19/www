a5614f6e7b0b05f774b05ca9381275d05c43ce4f
Siddharth Singh <siddharth.singh19@outlook.com>
2024-03-01 13:13:21 +0530

    done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import './footer.css'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'><FaEthereum/></a>
+      <a href="/" className='footer__logo'><FaEthereum/></a>
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
